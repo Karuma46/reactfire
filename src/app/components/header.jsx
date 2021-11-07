9cb681a5b7cca8840e8c8881272723f58c9e5f37
steve karuma <stevekaruma@gmail.com>
2021-11-07 14:17:38 +0300

    Header Components and Styling

--- src/app/components/header.jsx
+++ src/app/components/header.jsx
@@ -1,8 +1,30 @@
+import avatar from 'app/assets/img/avatar.png';
+
 const Header = () => {
   return(
-    <header>
-      <h1>React x Firebase</h1>
-      <h3>To do App</h3>
+    <header className="rb px-5 d-flex align-items-center justify-content-between">
+      <div className="">
+        <h1>React<b>Fire</b> </h1>
+        <h3>To do App <i className="bi-heart-fill"></i></h3>
+      </div>
+      
+      <div id="accountDetails">
+        <div id="accAvatar">
+          <img src={avatar} alt="" />
+        </div>
+        <div id="accInfo" className="text-center">
+          <h3>Steve Karuma</h3>
+          <div id="accActions">
+            <span>
+              <i className="bi-gear"></i>
+            </span>
+            <span>
+              <i className="bi-box-arrow-right"></i>
+            </span>
+          </div>
+        </div>
+      </div>
+
     </header>
   )
 }
