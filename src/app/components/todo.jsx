68a075f3b415c6b9a15b71197187f23562df3180
steve karuma <stevekaruma@gmail.com>
2021-11-07 12:11:07 +0300

    Moved todo file

--- src/app/components/todo.jsx
+++ src/app/components/todo.jsx
@@ -1,13 +1,18 @@
+import {doc} from "@firebase/firestore";
+import { db } from "app/firebase/config";
 import { useState, useEffect } from "react";
-import Api from "./firebase/api"
-import Addtask from "./components/addtask";
-import Task from "./components/task";
+import {useParams, Route} from "react-router-dom";
+import Api from "../firebase/api"
+import Addtask from "./addtask";
+import Task from "./task";
 
 const Todos = () => {
   const [tasks, setTasks] = useState([]);
+  const {id} = useParams();
 
   const getTasks = async () => {
-    Api.get('tasks')
+    var docRef = doc(db, 'lists', id)
+    Api.query('tasks', ['listid', '==', docRef])
     .then(res => {
       setTasks(res.docs.map(doc => ({...doc.data(), id:doc.id})))
     })
@@ -17,14 +22,14 @@ const Todos = () => {
   };
   useEffect(() => {
     getTasks();
-  }, []);
+  }, [id]);
 
   return (
     <>
       <div>
         <div className="tasks">
-          {tasks.map((task) => (
-            <Task key={task.id} task={task} getTasks={getTasks} />
+        {tasks.map((task) => (
+          <Task key={task.id} task={task} getTasks={getTasks} />
           ))}
         </div>
         <Addtask getTasks={getTasks} />
